4592e58401ff40413fd2fdcaa229538ae893fce6
enhui zhu <zhuen2000@163.com>
2020-06-01 22:31:12 +0100

    first commit

---
+++ src/main/java/olmarket/kidmath/KidmathApplication.java
@@ -0,0 +1,13 @@
+package olmarket.kidmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidmathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidmathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/olmarket/kidmath/KidmathApplicationTests.java
@@ -0,0 +1,13 @@
+package olmarket.kidmath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidmathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
